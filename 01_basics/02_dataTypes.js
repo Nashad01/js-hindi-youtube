f711169ea77a16d74ceb8070b96620fac2ee179e
Mohd Nashad Jilani <147087257+Nashad01@users.noreply.github.com>
2024-10-19 10:19:50 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS cod as newer version
+
+// alert (3 + 3) // we are using nodejs, not browser
+
+console.log( 3 + 3 ); 
+
+console.log("Hitesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+//nubmer => 2 to power 53
+// bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined);
