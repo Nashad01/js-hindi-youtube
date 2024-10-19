cacd54e7c34834c4c84a3670d9b46a4402448898
Mohd Nashad Jilani <147087257+Nashad01@users.noreply.github.com>
2024-10-19 17:59:53 +0000

    lecture 7 operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,6 +25,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
-console.log(typeof (stringNumber))      //This is perform by my side
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+// console.log(typeof (stringNumber))      //This is perform by my side
+
+//************************************* Operation *************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Ninja"
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3 );
+
+// console.log(true )
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+gameCounter++;                         //++gameCounter is prefix operator and gameCounter++ is postfix operator
+console.log(gameCounter);
